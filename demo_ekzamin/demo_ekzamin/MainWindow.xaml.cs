c3724d4d655855abfa68812387d89836e7af4ae7
Ваше имя <nothere@inthe.net>
2021-07-01 16:20:42 +0300

    asa

---
+++ demo_ekzamin/demo_ekzamin/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demo_ekzamin
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private List<spt_fallback_db> _ServiceList;
+        public List<spt_fallback_db> ServiceList
+        {
+            get { return _ServiceList; }
+            set { _ServiceList = value; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ServiceList = Core.DB.spt_fallback_db.ToList();
+        }
+    }
+    
+
+
+}
